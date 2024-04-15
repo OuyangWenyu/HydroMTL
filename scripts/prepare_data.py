1379d684cf0fc8f6001b18ab1c162bb29f71234a
ouyang,wenyu <wenyuouyang@outlook.com>
2024-04-15 19:46:22 +0800

    maybe new memory hardware is worse so the speed slow down; set pythonpath for prepare_data.py

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -1,7 +1,7 @@
 """
 Author: Wenyu Ouyang
 Date: 2022-01-08 16:58:14
-LastEditTime: 2024-04-14 18:19:15
+LastEditTime: 2024-04-15 19:42:00
 LastEditors: Wenyu Ouyang
 Description: Choose some basins for training and testing of multioutput exps
 FilePath: \HydroMTL\scripts\prepare_data.py
@@ -9,13 +9,18 @@ Copyright (c) 2021-2022 Wenyu Ouyang. All rights reserved.
 """
 
 import os
+import sys
+from pathlib import Path
 import pandas as pd
 import csv
 
 from hydrodataset import Camels
 from hydrodataset.hds_utils import usgs_screen_streamflow
-
 from torchhydro import SETTING
+
+
+dir_root = os.path.dirname(Path(os.path.abspath(__file__)).parent)
+sys.path.append(dir_root)
 
 # to avoid the effect from bad-quality streamflow data, we exclude sites with over 5% (about 1 year) nan values
 data_dir = SETTING["local_data_path"]["datasets-origin"]
@@ -29,8 +34,10 @@ selected_ids = usgs_screen_streamflow(
     **flow_screen_param
 )
 df_camels_mtl = pd.DataFrame({"GAGE_ID": selected_ids})
+if os.path.exists(os.path.join(dir_root, "results")) is False:
+    os.makedirs(os.path.join(dir_root, "results"))
 df_camels_mtl.to_csv(
-    os.path.join("results", "camels_us_mtl_2001_2021_flow_screen.csv"),
+    os.path.join(dir_root, "results", "camels_us_mtl_2001_2021_flow_screen.csv"),
     quoting=csv.QUOTE_NONNUMERIC,
     index=None,
 )
